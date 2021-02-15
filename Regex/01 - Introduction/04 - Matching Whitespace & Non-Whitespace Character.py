3a47853c92be635a3b543adbcd22e82b58bf378e
Nathan Abela <nathanabela7@gmail.com>
2021-02-15 10:45:03 +0100

    Update Regex Solutions Explanations

--- Regex/01 - Introduction/04 - Matching Whitespace & Non-Whitespace Character.py
+++ Regex/01 - Introduction/04 - Matching Whitespace & Non-Whitespace Character.py
@@ -18,11 +18,11 @@ regex_pattern = r'(\S{2}\s){2}\S{2}'
 # Regex Pattern:
 # .
 # ├── (\S{2}\s){2}
-# │   ├── \S{2} - Denotes any non-whitespace character for 2 times
-# │   ├── \s - Denotes any any whitespace character
+# │   ├── \S{2} - Denotes any non-whitespace character (equal to [^\r\n\t\f\v ]) for 2 times
+# │   ├── \s - Denotes any whitespace character (equal to [\r\n\t\f\v ])
 # │   └── {2} - Denotes the above expression for 2 times
 # └── \S{2}
-#     ├── \S{2} - Denotes any non-whitespace character for 2 times
+#     ├── \S - Denotes any non-whitespace character (equal to [^\r\n\t\f\v ]) for 2 times
 #     └── {2} - Denotes the above expression twice
 
 # Example: 18 11 20
--- Regex/03 - Repetitions/03 - Matching Zero Or More Repetitions.py
+++ Regex/03 - Repetitions/03 - Matching Zero Or More Repetitions.py
@@ -21,7 +21,7 @@ regex_pattern = r'^\d{2,}[a-z]*[A-Z]*$'
 # │   └── Denotes the start of the line
 # ├── \d{2,}
 # │   ├── \d - Denotes a digit (equal to [0-9])
-# │   └── {2} - Denotes the above expression for 2 or more times
+# │   └── {2,} - Denotes the above expression for 2 or more times
 # ├── [a-z]*
 # │   ├── a-z - Denotes a single character in the range of a and z
 # │   └── * - Denotes the above expression for 0 or unlimited times, same as {0,}
